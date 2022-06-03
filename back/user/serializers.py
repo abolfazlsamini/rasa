5e96757200f6d6b4dd35d63db04f6fb2c4e6f7ca
saminimaster <samini7a@gmail.com>
2022-06-03 06:14:45 -0930

    commit

--- back/user/serializers.py
+++ back/user/serializers.py
@@ -3,38 +3,25 @@ from rest_framework.response import Response
 from .models import Pages, Post, UserModel
 
 
-
 class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = ('username', 'password')
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        
         user = UserModel.objects.create_user(validated_data['username'], None,validated_data['password'])
         return user
-
-
-class GetUserPostsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserModel
-        fields = ('posts',)
+# User Register
         
 class PageSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Pages
         fields = ('id', 'page_title')
-
-
 class PostSerializer(serializers.ModelSerializer):
     pages = PageSerializer(many=True, read_only=True)
     class Meta:
         model = Post
         fields = ('id', 'post_title', 'pages')
-
-
+# GET user POSTS and PAGES
 
--- back/user/views.py
+++ back/user/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.generics import RetrieveAPIView, ListAPIView, CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .serializers import UserRegisterSerializer, GetUserPostsSerializer, PostSerializer
+from .serializers import UserRegisterSerializer, PostSerializer
 from .models import UserModel
 from post.models import Post
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -24,20 +24,8 @@ class UserRegisterView(CreateAPIView):
     # user get token after registering
 
 
-
 class GetUserPostsVIew(ListAPIView):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     
-    # def get_queryset(self):
-    #     try:
-    #         user = self.request.user
-    #         users_post = user.posts.first()
-    #         users_page = users_post.pages.all()
-    #         return Response(users_page)
-    #     except Exception as e:
-    #         return Response({"Error": str(e)})
-# class SubmitNewPostView(CreateAPIView):
-#     pass
-#     #serializer_class = SubmitNewPostSerializer
