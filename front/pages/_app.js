6395ba1bfe87d1cc45892cdba0655eea390a69a5
saminimaster <samini7a@gmail.com>
2022-08-29 00:09:46 +0430

    commit

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,22 +1,17 @@
-import Navbar from '../components/navbar'
+import Navbar from "../components/navbar";
 import React from "react";
-import store from '../store'
-import { Provider } from 'react-redux'
+import store from "../store";
+import { Provider } from "react-redux";
 
 function MainApp({ Component, pageProps }) {
   return (
     <React.Fragment>
-
       <Provider store={store}>
-
-        <Navbar/>
+        <Navbar />
         <Component {...pageProps} />
-        
       </Provider>
-
     </React.Fragment>
-  )
+  );
 }
 
-
-export default MainApp
+export default MainApp;
